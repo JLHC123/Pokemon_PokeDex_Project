211160cfbe1ce18692293e64d7d34c34153d4750
Jorge Hernandez Cruz <jlhc3751@gmail.com>
2025-05-16 14:30:51 -0700

    Created a new endpoint that gets the capturedPokemont that a specific user owns.

--- RandomStuff/Controllers/CapturedPokemonController.cs
+++ RandomStuff/Controllers/CapturedPokemonController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RandomStuff.Data;
+using RandomStuff.Models;
 
 namespace RandomStuff.Controllers
 {
@@ -15,6 +16,23 @@ namespace RandomStuff.Controllers
                 .Include(cp => cp.User)
                 .ToList();
             return Ok(allCapturedPokemon);
+        }
+
+        [HttpGet("GetUserCapturedPokemon")]
+        public IActionResult GetUserCapturedPokemon(long UserId)
+        {
+            PokemonContext _context = new PokemonContext();
+            var user = _context.user.Find(UserId);
+            if (user == null)
+            {
+                return NotFound($"User with ID {UserId} not found.");
+            }
+            var userCapturedPokemon = _context.capturedPokemon
+                .Where(cp => cp.UserId == UserId)
+                .OrderBy(cp => cp.CapturedPokemonId)
+                .Include(cp => cp.Pokemon)
+                .ToList();
+            return Ok(userCapturedPokemon);
         }
     }
 }
